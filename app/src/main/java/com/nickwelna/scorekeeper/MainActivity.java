1fca0497d33f944d1197a3b06484b84dfedc7ece
Nick Welna <welnanick@gmail.com>
2017-10-08 17:51:46 -0700

    master: Added reviewer suggestion, cleaned and updated build.gradle

--- app/src/main/java/com/nickwelna/scorekeeper/MainActivity.java
+++ app/src/main/java/com/nickwelna/scorekeeper/MainActivity.java
@@ -9,10 +9,15 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    public int score1 = 0;
-    public int score2 = 0;
-    public int penalty1 = 0;
-    public int penalty2 = 0;
+    private int score1 = 0;
+    private int score2 = 0;
+    private int penalty1 = 0;
+    private int penalty2 = 0;
+
+    private static final String SCORE_1 = "score 1";
+    private static final String SCORE_2 = "score 2";
+    private static final String PENALTY_1 = "penalty 1";
+    private static final String PENALTY_2 = "penalty 2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateScore1() {
 
-        TextView scores1 = (TextView) findViewById(R.id.score_1);
+        TextView scores1 = findViewById(R.id.score_1);
         scores1.setText(String.format(Locale.US, "%d", score1));
 
     }
@@ -43,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void updatePenalty1() {
 
-        TextView penalties1 = (TextView) findViewById(R.id.penalty_1);
+        TextView penalties1 = findViewById(R.id.penalty_1);
         penalties1.setText(String.format(Locale.US, "%d", penalty1));
 
     }
@@ -58,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateScore2() {
 
-        TextView scores2 = (TextView) findViewById(R.id.score_2);
+        TextView scores2 = findViewById(R.id.score_2);
         scores2.setText(String.format(Locale.US, "%d", score2));
 
     }
@@ -72,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void updatePenalty2() {
 
-        TextView penalties2 = (TextView) findViewById(R.id.penalty_2);
+        TextView penalties2 = findViewById(R.id.penalty_2);
         penalties2.setText(String.format(Locale.US, "%d", penalty2));
 
     }
@@ -89,4 +94,32 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(SCORE_1, score1);
+        outState.putInt(SCORE_2, score2);
+        outState.putInt(PENALTY_1, penalty1);
+        outState.putInt(PENALTY_2, penalty2);
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+
+        super.onRestoreInstanceState(savedInstanceState);
+
+        score1 = savedInstanceState.getInt(SCORE_1);
+        score2 = savedInstanceState.getInt(SCORE_2);
+        penalty1 = savedInstanceState.getInt(PENALTY_1);
+        penalty2 = savedInstanceState.getInt(PENALTY_2);
+
+        updateScore1();
+        updateScore2();
+        updatePenalty1();
+        updatePenalty2();
+
+    }
 }
